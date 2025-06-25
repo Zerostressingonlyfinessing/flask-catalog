4ebb62473c3f9d7f97a919c81d5dcb5da4ae54b9
Zerostressingonlyfinessing <ruslan.boulevard@yandex.ru>
2025-06-25 15:58:28 +0300

    Update config.py

--- config.py
+++ config.py
@@ -1,5 +1,5 @@
 class Config:
     SECRET_KEY = 'your_flask_secret_key'
-    ADMIN_PASSWORD = 'admin123'
+    ADMIN_PASSWORD = 'admin457181QWEqwe'
     SQLALCHEMY_DATABASE_URI = 'sqlite:///app.db'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
